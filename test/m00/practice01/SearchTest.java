92e124d84b91d8c42a6b3d2a4aeb2be0a6d1425b
EdgarsGars <edgars.garsneks@gmail.com>
2015-09-18 13:46:07 +0300

    Init commit

---
+++ test/m00/practice01/SearchTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m00.practice01;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Edgar_000
+ */
+public class SearchTest {
+    
+    public SearchTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
